f461ba3b9fececf779317d7520338d4aef4e3e53
Matias H. Nurmik <matiasnurmik@hotmail.com>
2024-12-18 21:18:34 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+Random rnd = new Random();
+
+
+int MyRandomNum;
+
+int randomSum = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    MyRandomNum = rnd.Next(0, 11); //0-10
+    randomSum = randomSum + MyRandomNum;
+    Console.WriteLine($"My random number is: {MyRandomNum}");
+}
+Console.WriteLine($"random sum total: {randomSum}");
